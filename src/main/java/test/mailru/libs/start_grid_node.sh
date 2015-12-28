9290a1b6374b4440175ab89a2c3622398c451014
cqi <cqi@home>
2015-12-28 17:41:58 +0300

    modify start node script

--- src/main/java/test/mailru/libs/start_grid_node.sh
+++ src/main/java/test/mailru/libs/start_grid_node.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup java -jar selenium-server-standalone-2.48.2.jar -role node -hub http://192.168.10.4:4444/grid/register &>/dev/null &
+java -jar selenium-server-standalone-2.48.2.jar -role node -hub http://192.168.10.4:4444/grid/register -browser browserName=firefox,version=43.0,platform=LINUX
