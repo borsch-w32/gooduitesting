8018ff9360f8264e970ca4124f95411c12005757
cqi <cqi@home>
2016-01-16 21:39:21 +0300

    refatoring part 2

--- start_grid_hub.sh
+++ start_grid_hub.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar selenium-server-standalone-2.48.2.jar -role hub -browser browserName=firefox,version=43.0,platform=LINUX
+java -jar selenium-server-standalone-2.49.0.jar -role hub -browser browserName=firefox,version=43.0,platform=LINUX
--- start_grid_node.sh
+++ start_grid_node.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar selenium-server-standalone-2.48.2.jar -role node -hub http://192.168.10.4:4444/grid/register -browser browserName=firefox,version=43.0,platform=LINUX
+java -jar /home/cqi/IdeaProjects/gooduitesting/src/main/java/ru/mail/libs/selenium-server-standalone-2.49.0.jar -role node -hub http://192.168.10.4:4444/grid/register -browser browserName=firefox,version=43.0,platform=LINUX
