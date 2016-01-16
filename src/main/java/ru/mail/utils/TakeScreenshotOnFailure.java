ccbc4d06abe7aabad18454bc2051bec9405ae054
cqi <cqi@home>
2016-01-17 00:52:16 +0300

    add screenshot taker

---
+++ src/main/java/ru/mail/utils/TakeScreenshotOnFailure.java
@@ -0,0 +1,27 @@
+package ru.mail.utils;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import ru.mail.pages.AbstractPage;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by cqi on 16.1.16. Educate. Grow. Satan.
+ */
+public class TakeScreenshotOnFailure extends AbstractPage
+{
+    public TakeScreenshotOnFailure(WebDriver webDriver)
+    {
+        super(webDriver);
+    }
+
+    public static void takeScreenshot(WebDriver webDriver) throws IOException
+    {
+        File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("/home/cqi/IdeaProjects/gooduitesting/target/surefire-reports/screenshots/failure_screenshot.png"), true);
+    }
+}
