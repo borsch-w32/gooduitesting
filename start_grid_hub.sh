d5561e314b800b1e4c97ddd916a4307eb81947ae
cqi <cqi@home>
2016-01-17 15:39:00 +0300

    update start grid scripts add nohup to start scripts in background and redirect output to /dev/null/

--- start_grid_hub.sh
+++ start_grid_hub.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar selenium-server-standalone-2.49.0.jar -role hub -browser browserName=firefox,version=43.0,platform=LINUX
+nohup java -jar selenium-server-standalone-2.49.0.jar -role hub -browser browserName=firefox,version=43.0,platform=LINUX > /dev/null 2>&1 &
--- start_grid_node.sh
+++ start_grid_node.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /home/cqi/IdeaProjects/gooduitesting/src/main/java/ru/mail/libs/selenium-server-standalone-2.49.0.jar -role node -hub http://192.168.10.4:4444/grid/register -browser browserName=firefox,version=43.0,platform=LINUX
+nohup java -jar /home/cqi/IdeaProjects/gooduitesting/src/main/java/ru/mail/libs/selenium-server-standalone-2.49.0.jar -role node -hub http://192.168.10.4:4444/grid/register -browser browserName=firefox,version=43.0,platform=LINUX > /dev/null 2>&1 &
