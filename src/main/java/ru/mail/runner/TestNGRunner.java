bf0abb5e0ebc0acc5c7c1a90ae6a7051d6ba83ef
cqi <cqi@home>
2016-01-17 16:26:40 +0300

    add custom test runner

---
+++ src/main/java/ru/mail/runner/TestNGRunner.java
@@ -0,0 +1,34 @@
+package ru.mail.runner;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cqi on 17.1.16. Educate. Grow. Satan.
+ */
+public class TestNGRunner
+{
+    public static void main(String[] args)
+    {
+        XmlSuite suite = new XmlSuite();
+        suite.setName("Test mail.ru");
+
+        XmlTest test = new XmlTest(suite);
+        test.setName("testMailRu");
+        List<XmlClass> classes = new ArrayList<XmlClass>();
+        classes.add(new XmlClass("ru.mail.tests.EmailOperationsTests"));
+        classes.add(new XmlClass("ru.mail.tests.PageComponentsTests"));
+        test.setXmlClasses(classes);
+
+        List<XmlSuite> suites = new ArrayList<XmlSuite>();
+        suites.add(suite);
+        TestNG testNG = new TestNG();
+        testNG.setXmlSuites(suites);
+        testNG.run();
+    }
+}
