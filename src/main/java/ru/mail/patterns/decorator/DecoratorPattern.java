3ea0f6cb0779f3f4c66efb06b04a9a1bc017f22a
Siarhei Cyargeenka1 <Siarhei_Cyargeenka1@epam.com>
2016-01-29 16:24:18 +0300

    add logger to decorator

--- src/main/java/ru/mail/patterns/decorator/DecoratorPattern.java
+++ src/main/java/ru/mail/patterns/decorator/DecoratorPattern.java
@@ -1,25 +1,22 @@
 package ru.mail.patterns.decorator;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 import ru.mail.pages.PersonalAccountPage;
 import ru.mail.utils.PropertiesParser;
-
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by Sergei_Sergeenko on 1/29/2016.
  */
-public class DecoratorPattern extends PersonalAccountPage {
-
-    private static final Logger LOGGER = Logger.getLogger(DecoratorPattern.class.getName());
-    Handler consoleHandler = null;
+public class DecoratorPattern extends PersonalAccountPage
+{
+    private static Logger logger = Logger.getLogger(DecoratorPattern.class.getName());
 
     @FindBy(name = "Login")
     private WebElement login;
@@ -30,9 +27,10 @@ public class DecoratorPattern extends PersonalAccountPage {
     @FindBy(id = "mailbox__auth__button")
     private WebElement authButton;
 
-    protected PersonalAccountPage  personalAccountPage;
+    protected PersonalAccountPage personalAccountPage;
 
-    public DecoratorPattern(WebDriver webDriver) {
+    public DecoratorPattern(WebDriver webDriver)
+    {
         super(webDriver);
     }
 
@@ -42,13 +40,7 @@ public class DecoratorPattern extends PersonalAccountPage {
         login.sendKeys(propertiesParser.getLogin());
         passwd.sendKeys(propertiesParser.getPassword());
         authButton.click();
-        consoleHandler = new ConsoleHandler();
-        LOGGER.addHandler(consoleHandler);
-        consoleHandler.setLevel(Level.ALL);
-        LOGGER.setLevel(Level.ALL);
-        LOGGER.info("Logger Name: "+LOGGER.getName());
-        return PageFactory.initElements(webDriver, PersonalAccountPage.class);
+        logger.info("Logged into personal account");
+        return PageFactory.initElements(this.webDriver, PersonalAccountPage.class);
     }
 }
-
-
