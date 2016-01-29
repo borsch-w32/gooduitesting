afc6f4d7a0e76b51e87e751539aedef88b43ceac
Siarhei Cyargeenka1 <Siarhei_Cyargeenka1@epam.com>
2016-01-29 16:24:34 +0300

    add test which uses decorator

---
+++ src/main/java/ru/mail/tests/EmailTestsUsingDecorator.java
@@ -0,0 +1,55 @@
+package ru.mail.tests;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ru.mail.pages.PersonalAccountPage;
+import ru.mail.patterns.decorator.DecoratorPattern;
+import ru.mail.patterns.singleton.WebDriverSingleton;
+import ru.mail.utils.PropertiesParser;
+
+/**
+ * Created by Sergei_Sergeenko on 1/29/2016.
+ */
+public class EmailTestsUsingDecorator
+{
+    private WebDriver webDriver;
+
+    @BeforeMethod(description = "Open new page")
+    public void setUp() throws IOException
+    {
+        PropertiesParser propertiesParser = new PropertiesParser();
+        webDriver = WebDriverSingleton.getWebDriverInstance();
+        webDriver.get(propertiesParser.getSite());
+        webDriver.manage().window().maximize();
+    }
+
+    @Test(testName = "Check personal mail hyperlink")
+    public void testHyperLink() throws IOException
+    {
+        PersonalAccountPage personalAccountPage = new PersonalAccountPage(webDriver);
+        personalAccountPage = new DecoratorPattern(webDriver);
+        personalAccountPage.enterData();
+        WebElement userEmailHyperlink = (new WebDriverWait(webDriver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("PH_user-email")));
+        String hyperLink = webDriver.findElement(By.id("PH_user-email")).getText();
+        assertEquals(hyperLink, "borsch.w32@mail.ru");
+    }
+
+    @AfterMethod(description = "Close FF Instance")
+    public void tearDown()
+    {
+        WebDriverSingleton webDriverSingleton = new WebDriverSingleton();
+        webDriverSingleton.tearDown();
+    }
+}
